d76c40623609a491828abf2789a5c7977c4705b8
shiperl <shiperl@ufl.edu>
2017-03-19 17:04:14 -0400

    Add files via upload

---
+++ PythonScripts/ConvertToValidAlias.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+
+import csv
+import os
+import re
+
+def getLines(filePath):
+    rows = []
+    with open(filePath, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
+        for row in reader:
+            rows.append(row)
+    return rows
+    
+def readLines(arr):
+    for line in arr:
+        print(line)
+        
+def createHeaderInsertLine():
+    val = "SET DEFINE OFF\nINSERT ALL \n"
+    return val
+    
+def createValuesLine(line, num, name):
+    val = "INTO " + name + " VALUES "
+    val = val + "('" + str(num) + "',"
+    val = val + '\',\''.join(line)
+    val = val + "')"
+    return val
+
+def main():
+    print("Please enter the filepath of the file you would like to transform")
+    filepath = input()
+    #filepath = 'INMATE_ACTIVE_ALIASES.csv'
+    lines = getLines(filepath)
+    
+    print("What is the name of the file that you would like to produce?")
+    targetfile = input()
+    f = open(targetfile, 'w')
+    f.write(createHeaderInsertLine())
+    
+    print("What is the table name?")
+    name = input()
+    
+
+    tLines = []
+    currNum = 1
+
+    for line in lines:
+        tLines.append(createValuesLine(line, currNum, name))
+        currNum += 1
+    
+    f.write(' \n'.join(tLines))
+    f.write('\nSELECT * FROM dual;')
+    
